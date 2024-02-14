f8aefabe25a69689e90f850d79dca104853e77bb
Mayra Denhoff <mayradenhoff@gmail.com>
2024-02-14 10:54:32 -0300

    readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # email-verification
+
+1. Luego de clonar el repositorio, debes crear un entorno de desarrollo.
+2. Debes instalar las dependencias con el siguiente comando:
+    ```install -r requirements.txt```
+3. Crear tu base de datos.
+4. Levantar el servidor con el siguiente comando:
+    ``` uvicorn main:app --reload```
+
+
+Documentacion:
+    localhost:PORT/docs
--- config/db.py
+++ config/db.py
@@ -1,20 +1,12 @@
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm.session import sessionmaker
-
-# DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/testdb"
-
-# engine = create_engine(DATABASE_URL, echo=True)
-
-# Session = sessionmaker(bind=engine)
-
-# Base = declarative_base()
-
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm.session import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+load_dotenv()
 
-database_url = "postgresql://postgres:postgres@localhost:5433/testdb"
+db = os.getenv("db")
+database_url = db
 
 engine = create_engine(database_url)
 
